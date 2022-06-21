dde0b7fc2daa0bf0c3a2d69d99d87075420a00be
MarkQi2002 <qiji2002714@gmail.com>
2022-06-20 22:24:56 -0700

    converted contact into contact us and fixed some html related issues

---
+++ client/waitingRoom/contactus.js
@@ -0,0 +1,13 @@
+/* Function To Save The User Input*/
+function saveMessage() {
+    var userInput = "First Name: ";
+    userInput += document.getElementById("firstName").value;
+    userInput += ";Second Name: ";
+    userInput += document.getElementById("lastName").value;
+    userInput += ";Email: ";
+    userInput += document.getElementById("email").value;
+    userInput += ";Subject: ";
+    userInput += document.getElementById("subject").value;
+
+    alert("Mail Sent Successfully")
+}
