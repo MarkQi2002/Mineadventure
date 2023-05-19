1c4ab4c8c816e38aaaffc20b0793949b5741161c
Mark Qi <mark.qi@mail.utoronto.ca>
2023-05-19 15:37:53 -0400

    Reorganized /server/dataStructure/binaryTree.js

--- server/dataStructure/binaryTree.js
+++ server/dataStructure/binaryTree.js
@@ -1,140 +1,94 @@
-// Node class
-class Node
-{
-    // Node Constructor
-    constructor(data)
-    {
+// JavaScript Have Garbage Collection Properties
+// Binary Tree Node Class
+class Node {
+    // Binary Tree Node Constructor
+    constructor(data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
 }
 
-// Binary Search tree class
-class BinarySearchTree
-{
-    // BinarySearchTree Constructore
-    constructor()
-    {
-        // Root Of a Binary Search Tree
+// Binary Search Tree Class
+class BinarySearchTree{
+    // BinarySearchTree Constructor
+    constructor() {
+        // Binary Search Tree Root
         this.root = null;
     }
  
-    // Function To Be Implemented
-    
-    // Helper Method Which Creates A New Node To
-    // Be Inserted And Calls insertNode
-    insert(data){
-        // Creating A Node And Initialising
-        // With Data
+    // Helper Method Which Creates A New Node To Be Inserted And Call insertNode
+    insert(data) {
+        // Creating A Node Initialzied With Data
         var newNode = new Node(data);
                         
-        // Root Is Null Then Node Will
-        // Be Added To The Tree And Made Root.
-        if(this.root === null)
+        // Empty BST -> Make newNode Root
+        if (this.root === null) {
             this.root = newNode;
-        else
-            // Find The Correct Position In The
-            // Tree And Add The Node
+        // Non Empty BST -> Insert newNode Into BST
+        } else {
             this.insertNode(this.root, newNode);
+        }
     }
     
-    // Method To Insert A Node In A Tree
-    // it moves over the tree to find the location
-    // to insert a node with a given data
-    insertNode(node, newNode){
-        // if the data is less than the node
-        // data move left of the tree
-        if(newNode.data < node.data)
-        {
-            // if left is null insert node here
-            if(node.left === null)
+    // Recursive Method To Insert BST Node Into BST
+    insertNode(node, newNode) {
+        // Data Less Than Current Node
+        if (newNode.data < node.data) {
+            if (node.left === null) {
                 node.left = newNode;
-            else
-                // if left is not null recur until
-                // null is found
+            } else {
                 this.insertNode(node.left, newNode);
-        }
-    
-        // if the data is more than the node
-        // data move right of the tree
-        else
-        {
-            // if right is null insert node here
-            if(node.right === null)
+            }
+        // Data Greater Or Equal To Current Nodes
+        } else {
+            if (node.right === null) {
                 node.right = newNode;
-            else
-                // if right is not null recur until
-                // null is found
+            } else {
                 this.insertNode(node.right,newNode);
+            }
         }
     }
 
-
-    /// removeNode with a given data
+    // Remove Node With Certain Data
     remove(data){
-        // root is re-initialized with
-        // root of a modified tree.
+        // removeNode -> Recursive Remove Method
+        // this.root -> New BST
         this.root = this.removeNode(this.root, data);
     }
     
-    // Method to remove node with a
-    // given data
-    // it recur over the tree to find the
-    // data and removes it
+    // Recursive Node Deletion Method
     removeNode(node, key){
-            
-        // if the root is null then tree is
-        // empty
-        if(node === null)
+        // Empty BST
+        if(node === null) {
             return null;
-    
-        // if data to be delete is less than
-        // roots data then move to left subtree
-        else if(key < node.data)
-        {
+        // Data Less Than Current Node
+        } else if(key < node.data) {
             node.left = this.removeNode(node.left, key);
             return node;
-        }
-    
-        // if data to be delete is greater than
-        // roots data then move to right subtree
-        else if(key > node.data)
-        {
+        // Data Greate Than Current Node
+        } else if(key > node.data) {
             node.right = this.removeNode(node.right, key);
             return node;
-        }
-    
-        // if data is similar to the root's data
-        // then delete this node
-        else
-        {
-            // deleting node with no children
-            if(node.left === null && node.right === null)
-            {
+        // Remove BST Node With Exact Same Data
+        } else {
+            // BST Node Without Children
+            if (node.left === null && node.right === null) {
                 node = null;
                 return node;
-            }
-    
-            // deleting node with one children
-            if(node.left === null)
-            {
+            // BST Node With One Left Child
+            } if (node.left === null) {
                 node = node.right;
                 return node;
-            }
-            
-            else if(node.right === null)
-            {
+            // BST Node With One Right Child
+            } else if (node.right === null) {
                 node = node.left;
                 return node;
             }
-    
-            // Deleting node with two children
-            // minimum node of the right subtree
-            // is stored in aux
+            
+            // BST Node With Two Children, Move Minimum Node In Right Subtree To Current Node
             var aux = this.findMinNode(node.right);
             node.data = aux.data;
-    
             node.right = this.removeNode(node.right, aux.data);
             return node;
         }
@@ -143,49 +97,39 @@ class BinarySearchTree
     // Return InOrder List
     getInorderList(){
         var newList = [];
-        this.inorder(this.root, newList);
+        this.inOrder(this.root, newList);
         return newList;
     }
 
-    // InOrder Helper Function
-    inorder(node, newList){
-        if(node !== null)
-        {
-            this.inorder(node.left, newList);
+    // Recursive InOrder Helper Method
+    inOrder(node, newList){
+        if (node !== null) {
+            this.inOrder(node.left, newList);
             newList.push(node.data);
-            this.inorder(node.right, newList);
+            this.inOrder(node.right, newList);
         }
     }
 
-    // finds the minimum node in tree
-    // searching starts from given node
-    findMinNode(node){
-        // if left of a node is null
-        // then it must be minimum node
+    // Method To Find Minimum BST Node
+    findMinNode(node) {
         if (node.left === null)
             return node;
         else
             return this.findMinNode(node.left);
     }
 
-    // search for a node with given data
+    // Method To Search A Node With Certain Data
     search(node, data){
-    // if trees is empty return null
-        if(node === null)
+        // Empty BST
+        if (node === null)
             return null;
-    
-        // if data is less than node's data
-        // move left
+        // Data Less Than Current Node Data
         else if(data < node.data)
             return this.search(node.left, data);
-    
-        // if data is less than node's data
-        // move left
+        // Data Greater Than Current Node Datas
         else if(data > node.data)
             return this.search(node.right, data);
-    
-        // if data is equal to the node data
-        // return node
+        // Data Equal To Current Node Data
         else
             return node;
     }
